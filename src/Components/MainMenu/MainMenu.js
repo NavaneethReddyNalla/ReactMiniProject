ea1c6a4daf0882ac904da1292ffbba92f334f0a1
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-09 23:49:26 +0530

    Initialize all components

---
+++ src/Components/MainMenu/MainMenu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function MainMenu() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      let prods = await fetch("https://fakestoreapi.com/products");
+      prods = await prods.json();
+      setProducts(prods);
+    }
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      {products.map((product) => {
+        return <p key={product.id}>{JSON.stringify(product)}</p>;
+      })}
+    </div>
+  );
+}
+
+export default MainMenu;
