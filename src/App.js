4aa142000bd8b824932ffdf51015ec54575d5eaf
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-11 00:52:50 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 import MainMenu from "./Components/MainMenu/MainMenu";
 
 function App() {
   return (
-    <div className="container-fluid App">
-      <MainMenu />
+    <div className="App">
+      <Header />
+      <div className="container-fluid">
+        <MainMenu />
+      </div>
     </div>
   );
 }
